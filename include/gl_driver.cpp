9877f743b0ec07f0e6e5b9d6d5a5c30bc023fa51
smkang <sungmo.kang@soslab.co>
2020-07-13 09:47:26 +0900

    add Thread

--- include/gl_driver.cpp
+++ include/gl_driver.cpp
@@ -14,9 +14,34 @@
 #define PE              0xC2
 
 
+#define STATE_INIT      0
+#define STATE_PS1       1
+#define STATE_PS2       2
+#define STATE_PS3       3
+#define STATE_PS4       4
+#define STATE_preDATA   5
+#define STATE_PE        6
+#define STATE_CS        7
+
+
+int recv_state = STATE_INIT;
+
+std::vector<uint8_t> recv_packet;
+std::vector<uint8_t> recv_data;
+int recv_TL = 0;
+int recv_SM = 0;
+int recv_CAT0 = 0;
+int recv_CAT1 = 0;
+int recv_DTL = 0;
+
+std::vector<uint8_t> lidar_data;
+std::vector<uint8_t> serial_num;
+
 		
 serial::Serial * port_;
 uint8_t cs_;
+uint8_t write_cs_;
+
 
 //////////////////////////////////////////////////////////////
 // Constructor and Deconstructor for GL Class
@@ -34,14 +59,20 @@ Gl::Gl()
 
 Gl::~Gl()
 {
+    SetFrameDataEnable(false);
+
     port_->close();
     delete port_;
+    thread_running = false;
 }
+
 
 void Gl::OpenSerial(std::string &port, uint32_t baudrate)
 {
-    port_ = new serial::Serial(port, baudrate, serial::Timeout::simpleTimeout(100));
+    port_ = new serial::Serial(port, baudrate, serial::Timeout::simpleTimeout(10));
     if(port_->isOpen()) std::cout << "GL Serial is opened." << std::endl;
+
+    th = std::thread(&Gl::ThreadCallBack,this);
 }
 
 
@@ -72,7 +103,7 @@ void cs_clear()
 void write(uint8_t data)
 {
     port_->write(&data, 1);
-    cs_update(data);
+    write_cs_ = write_cs_^ (data&0xff);
 }
 
 bool read(uint8_t *data)
@@ -81,7 +112,7 @@ bool read(uint8_t *data)
     if (port_->read(&buff, 1) == 1)
     {
         *data = buff;
-        cs_update(buff);
+        // cs_update(buff);
         return true;
     }
     else
@@ -100,7 +131,8 @@ void write_PS()
 void write_packet(uint8_t PI, uint8_t PL, uint8_t SM, uint8_t CAT0, uint8_t CAT1, std::vector<uint8_t> DTn)
 {
     flush();
-    cs_clear();
+
+    write_cs_ = 0;
 
     write_PS();
 
@@ -123,97 +155,172 @@ void write_packet(uint8_t PI, uint8_t PL, uint8_t SM, uint8_t CAT0, uint8_t CAT1
 
     write(PE);
 
-    write(cs_get());
+    write(write_cs_&0xff);
 }
 
-bool check_PS(void)
+
+void recv_packet_clear(void)
 {
     cs_clear();
+    recv_state = STATE_INIT;
 
-    uint8_t data;
-    while(read(&data)) 
+    recv_packet.clear();
+    recv_TL = 0;
+    recv_SM = 0;
+    recv_CAT0 = 0;
+    recv_CAT1 = 0;
+    recv_DTL = 0;
+    recv_data.clear();
+}
+
+
+int check_PS(uint8_t data)
+{
+    if(recv_state==STATE_INIT)
     {
         if(data==PS1)
         {
-            if(!read(&data)) return false;
-            if(data==PS2)
-            {
-                if(!read(&data)) return false;
-                if(data==PS3)
-                {
-                    if(!read(&data)) return false;
-                    if(data==PS4)
-                    {
-                        return true;
-                    }
-                }    
-            }
+            recv_packet_clear();
+            cs_update(data);
+            recv_state = STATE_PS1;
         }
-        cs_clear();
     }
-    
-    return false;
-}
-
-std::vector<uint8_t> read_packet(uint8_t SM_check, uint8_t CAT0_check, uint8_t CAT1_check)
-{
-    std::vector<uint8_t> packet_data;
-
-    while(check_PS())
+    else if(recv_state==STATE_PS1)
     {
-        packet_data.clear();
-
-        uint8_t buff;
-        if(!read(&buff)) return packet_data;
-        uint16_t TL = buff&0xff;
-        if(!read(&buff)) return packet_data;
-        TL |= ((uint16_t)(buff&0xff))<<8;
-
-        if(!read(&buff)) return packet_data;
-        if(!read(&buff)) return packet_data;
-           
-        if(!read(&buff)) return packet_data;
-        uint8_t SM = buff&0xff;
-            
-        if(!read(&buff) || (BI_GL3102PC!=(buff&0xff))) return packet_data;
-            
-        if(!read(&buff)) return packet_data;
-        uint8_t CAT0 = buff&0xff;
-        
-        if(!read(&buff)) return packet_data;
-        uint8_t CAT1 = buff&0xff;
-
-        uint16_t DTL = TL - 14;
-            
-        for(int i=0; i<DTL; i++)
+        if(data==PS2)
         {
-            if(!read(&buff))
-            {
-                packet_data.clear();
-                return packet_data;
-            }
-            packet_data.push_back(buff);
+            cs_update(data);
+            recv_state = STATE_PS2;
         }
-            
-        if(!read(&buff) || (PE!=(buff&0xff)))
+        else return 0;
+    }
+    else if(recv_state==STATE_PS2)
+    {
+        if(data==PS3)
         {
-            packet_data.clear();
-            return packet_data;
-        } 
-
-        uint8_t cs = cs_get();
-        if(!read(&buff) || (cs!=(buff&0xff)))
-        {
-            packet_data.clear();
-            return packet_data;
+            cs_update(data);
+            recv_state = STATE_PS3;
         }
+        else return 0;
+    }
+    else if(recv_state==STATE_PS3)
+    {
+        if(data==PS4)
+        {
+            cs_update(data);
+            recv_state = STATE_PS4;
+        }
+        else return 0;
+    }
+    else return 2;
 
-        if((SM==SM_check) && (CAT0==CAT0_check) && (CAT1==CAT1_check)) return packet_data;
+    return 1;
+}
+
+
+void save_data(std::vector<uint8_t> recv_data, int SM, int CAT0, int CAT1)
+{
+    // GetSerialNum()
+    if(SM==SM_GET && CAT0==0x02 && CAT1==0x0A)
+    {
+        serial_num = recv_data;
+    }
+    // ReadFrameData()
+    else if(SM==SM_STREAM && CAT0==0x01 && CAT1==0x02)
+    {
+        lidar_data = recv_data;
     }
 
-    packet_data.clear();
-    return packet_data;
 }
+
+
+void add_packet_element(uint8_t data)
+{
+    int PS_result = check_PS(data);
+    if(PS_result==0)
+    {
+        recv_packet_clear();
+        if(data==PS1) recv_state = STATE_PS1;
+    }
+    else if(PS_result==2)
+    { 
+        if(recv_state==STATE_PS4)
+        {
+            cs_update(data);
+            recv_packet.push_back(data);
+
+            if(recv_packet.size()==6 and recv_packet[5]!=BI_GL3102PC)
+            {
+                std::vector<uint8_t> packet = recv_packet;
+                recv_packet_clear();
+                for(int i=0; i<packet.size(); i++) add_packet_element(packet[i]);
+            }
+
+            if(recv_packet.size()==9)
+            {
+                recv_TL = recv_packet[0]&0xff;
+                recv_TL = recv_TL | ((recv_packet[1]&0xff)<<8);
+
+                recv_SM = recv_packet[4]&0xff;
+
+                recv_CAT0 = recv_packet[6]&0xff;
+                recv_CAT1 = recv_packet[7]&0xff;
+                    
+                recv_DTL = recv_TL - 14;
+            }
+
+            if(recv_DTL>0)
+            {
+                if(recv_DTL>recv_data.size()) recv_data.push_back(data);
+                else recv_state = STATE_preDATA;
+            }
+
+            if(recv_state==STATE_preDATA)
+            {
+                if(data==PE) recv_state = STATE_PE;
+                else
+                {
+                    std::vector<uint8_t> packet = recv_packet;
+                    recv_packet_clear();
+                    for(int i=0; i<packet.size(); i++) add_packet_element(packet[i]);
+                }
+            }
+        }
+        else if(recv_state==STATE_PE)
+        {
+            if(data==cs_get())
+            {
+                save_data(recv_data, recv_SM, recv_CAT0, recv_CAT1);
+                recv_packet_clear();
+            }
+            else
+            {
+                std::vector<uint8_t> packet = recv_packet;
+                recv_packet_clear();
+                for(int i=0; i<packet.size(); i++) add_packet_element(packet[i]);
+            }
+        }
+    }
+}
+
+
+void Gl::ThreadCallBack(void) 
+{
+    flush();
+    recv_packet_clear();
+
+    while(thread_running==true)
+    {
+        uint8_t data;
+        while(read(&data))
+        {
+            add_packet_element(data);
+        }
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    }
+}
+
 
 //////////////////////////////////////////////////////////////
 // Read GL Conditions
@@ -226,20 +333,22 @@ std::string Gl::GetSerialNum(void)
     uint8_t SM = SM_GET;
     uint8_t CAT0 = 0x02;
     uint8_t CAT1 = 0x0A;
-
     std::vector<uint8_t> DTn = {1};
-    write_packet(PI, PL, SM, CAT0, CAT1, DTn);
 
-    for(int i=0; i<100; i++)
+    serial_num.clear();
+    for(int i=0; i<50; i++)
     {
-        std::vector<uint8_t> packet_data = read_packet(SM, CAT0, CAT1);
-        if(packet_data.size()>0)
+        write_packet(PI, PL, SM, CAT0, CAT1, DTn);
+        
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+        if(serial_num.size()>0)
         {
-            std::string out_str(packet_data.begin(), packet_data.end());
+            std::string out_str(serial_num.begin(), serial_num.end());
             return out_str;
         }
     }
-    
+
     std::string out_str = "[ERROR] Serial Number is not received.";
     return out_str;
 }
@@ -273,15 +382,12 @@ Gl::framedata_t Gl::ReadFrameData(void)
 {
     Gl::framedata_t frame_data;
 
-    uint8_t SM = SM_STREAM;
-    uint8_t CAT0 = 0x01;
-    uint8_t CAT1 = 0x02;
-
-    std::vector<uint8_t> packet_data = read_packet(SM, CAT0, CAT1);
-    if(packet_data.size()>0)
+    if(lidar_data.size()>0)
     {
-        frame_data = ParsingFrameData(packet_data);
+        frame_data = ParsingFrameData(lidar_data);
     }
+
+    lidar_data.clear();
 
     return frame_data;
 }
--- include/gl_driver.h
+++ include/gl_driver.h
@@ -9,6 +9,7 @@
 #include <chrono>
 #include <thread>
 #include <iostream>
+#include <iomanip>
 #include <vector>
 #include <math.h>
 
@@ -17,24 +18,29 @@
 
 class Gl
 {
-	public:
-		typedef struct _FRAMEDATA_T
-		{
-			std::vector<double> angle;
-			std::vector<double> distance;
-			std::vector<double> pulse_width;
-		} framedata_t;
+public:
+	struct framedata_t
+	{
+		std::vector<double> angle;
+		std::vector<double> distance;
+		std::vector<double> pulse_width;
+	};
 
-	public:
-		Gl(std::string &port, uint32_t baudrate);
-		Gl();
-		~Gl();
+public:
+	Gl(std::string &port, uint32_t baudrate);
+	Gl();
+	~Gl();
 
-		void OpenSerial(std::string &port, uint32_t baudrate);
+	void OpenSerial(std::string &port, uint32_t baudrate);
 
-		std::string GetSerialNum(void);
-		framedata_t ReadFrameData(void);
-		void SetFrameDataEnable(uint8_t framedata_enable);
+	std::string GetSerialNum(void);
+	framedata_t ReadFrameData(void);
+	void SetFrameDataEnable(uint8_t framedata_enable);
+
+private:
+	void ThreadCallBack(void);
+	bool thread_running = true;
+	std::thread th;
 };
 
 #endif
